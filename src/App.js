3d1d055f3baaf1e08a7ecf46bc5081c208d79d48
Resham <reshmap@iprogrammer.co>
2022-12-25 23:50:03 +0530

    spin complete

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ let linkData = "U2FsdGVkX19p15GCzPYVtx7DwPAwldwewsUcC%2F%2BNe8ZEnIrdgUPZo0q3HGFo
   alert(linkData)
   if(linkData){
 
-    let bytes = CryptoJS.AES.decrypt(linkData,'VE1LLVNFRUQtRU5DLURFQw==')
+    let bytes = CryptoJS.AES.decrypt("U2FsdGVkX19p15GCzPYVtx7DwPAwldwewsUcC%2F%2BNe8ZEnIrdgUPZo0q3HGFokCDCSDGMInageg%2FuuhYo98OCgA%3D%3D",'VE1LLVNFRUQtRU5DLURFQw==')
       // let decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
           alert(bytes)
           // alert(decryptedData.msisdn)
