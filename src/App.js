643c5736c8915a966edfb4f761b828154f595a5d
Resham <reshmap@iprogrammer.co>
2022-12-26 12:32:41 +0530

    spin complete

--- src/App.js
+++ src/App.js
@@ -9,33 +9,18 @@ import {
 } from 'react-router-dom';
 import RewardHistory from './components/RewardHistory';
 import React, { useEffect } from 'react'
-// import CryptoJS from "crypto-js";
-var CryptoJS = require("crypto-js");
+import CryptoJS from "crypto-js";
 
 
 function App() {
 // let linkData = document.location.href.split('=')?.[1]
 let linkDatas = "U2FsdGVkX19p15GCzPYVtx7DwPAwldwewsUcC%2F%2BNe8ZEnIrdgUPZo0q3HGFokCDCSDGMInageg%2FuuhYo98OCgA%3D%3D"
-  // let {msidin}  = useParams();
-  // if(linkData){
     let linkData = decodeURIComponent(linkDatas); 
-    alert(linkDatas)
-
     let bytes = CryptoJS.AES.decrypt(linkData,'VE1LLVNFRUQtRU5DLURFQw==')
       let decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
-          alert(decryptedData)
-          // alert(decryptedData.msisdn)
-  // }
+          alert(JSON.stringify(decryptedData))
+          alert(decryptedData.msisdn)
 
-
-  // useEffect(() => {
-  //   return () => {
-  //     document.addEventListener("message", function(data) {
-  //       alert("i", data)
-  //       alert(data.data);
-  //       });
-  // }
-  // }, [])
 
 
   const btnClick = () => {
