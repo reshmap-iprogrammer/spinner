17d818baa3c20d6c6a23f256d4782855eabcf841
Resham <reshmap@iprogrammer.co>
2022-12-26 17:12:19 +0530

    spin complete

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ let linkDatas = "U2FsdGVkX19p15GCzPYVtx7DwPAwldwewsUcC%2F%2BNe8ZEnIrdgUPZo0q3HGF
 
   const btnClick = async () => {
     const rewardResponse = await getRequestData(route["GET_REWARD_HISTORY"]);
-    window.ReactNativeWebView.postMessage('Data from WebView / Website');
+    // window.ReactNativeWebView.postMessage('Data from WebView / Website');
     window.ReactNativeWebView.postMessage('Data from WebView / Website',rewardResponse?.data?.user_reward_count);
   }
 
