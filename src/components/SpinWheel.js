6dc2060fecc97fd27b1dc23e9a94c47c2491905f
Resham <reshmap@iprogrammer.co>
2023-01-02 23:57:34 +0530

    spin complete

--- src/components/SpinWheel.js
+++ src/components/SpinWheel.js
@@ -32,7 +32,8 @@ function SpinWheel() {
   let bytes = CryptoJS.AES.decrypt(linkData, 'VE1LLVNFRUQtRU5DLURFQw==')
   let decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
   let msisdn = JSON.stringify(decryptedData.msisdn)
-  alert(JSON.stringify(decryptedData.msisdn))
+  alert(Number(msisdn))
+  let msidnNumber = Number(msisdn)
 
   let timer;
   useEffect(() => {
@@ -55,7 +56,7 @@ function SpinWheel() {
 
   const getRewardCount = async () => {
     const rewardResponse = await getRequestData(
-      `${route["GET_REWARD_HISTORY"]}?user_profile_id=${msisdn}&spin_id=1&claim_status=0&rank=0`
+      `${route["GET_REWARD_HISTORY"]}?user_profile_id=${msidnNumber}&spin_id=1&claim_status=0&rank=0`
     );
     setRewardCount(rewardResponse?.data?.user_reward_count)
     setSpinData(rewardResponse?.data?.user_reward_count)
