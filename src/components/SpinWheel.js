f319095e61ed45d77d4ffdfb9e33835e4552bd5e
Resham <reshmap@iprogrammer.co>
2023-01-03 13:40:38 +0530

    spin complete

--- src/components/SpinWheel.js
+++ src/components/SpinWheel.js
@@ -27,60 +27,49 @@ function SpinWheel() {
   const [isCopied, setCopied] = useState(false);
   const navigate = useNavigate();
 
-  let msisdn = 765765;
-  // let linkDatas = document.location.href.split('data=').pop()
-  // let linkData = decodeURIComponent(linkDatas);
-  // if(linkData?.length){
-  //   let bytes = CryptoJS.AES.decrypt(linkData, 'VE1LLVNFRUQtRU5DLURFQw==')
-  //     let decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
-  //      msisdn = JSON.stringify(decryptedData)
-  //     // alert(JSON.stringify(decryptedData))
-  //     // alert(decryptedData.msisdn)
-  //   console.log('object', msisdn)
-  // }
-  // let bytes = CryptoJS.AES.decrypt(linkData, 'VE1LLVNFRUQtRU5DLURFQw==')
-  // let decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
-  // let msisdn = JSON.parse(decryptedData.msisdn)
+  let linkDatas = "U2FsdGVkX19sESFoX3uSxMSg9zOEHugGWVFhYpWW2hIIL5RFNzh8bXEFv5Lult9P%2BYRI%2FoX7aGvG0tYIH3ypew%3D%3D"
+  let linkData = decodeURIComponent((linkDatas));
+  let bytes = CryptoJS.AES.decrypt(linkData, 'VE1LLVNFRUQtRU5DLURFQw==')
+  let decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
   // alert(JSON.stringify(decryptedData))
-  // let circleId = JSON.stringify(decryptedData.circleId);
-  // let claimStatus = JSON.stringify(decryptedData.claim_status)
 
   let timer;
   useEffect(() => {
     spinWheelApi();
+    getRewardCount();
     getFlag();
     return () => {
       clearTimeout(timer);
     }
   }, []);
 
+  const copy = () => {
+    setCopied(true)
+  }
 
   const spinWheelApi = async () => {
     const response = await getRequestData(route["GET_SPIN"]);
     setSpinnerValues(response?.data?.SpinWheelCouponData)
   }
 
-  const getFlag = async () => {
-    const getFlagresponse = await getRequestData(`${route["GET_REWARD_HISTORY_FLAG"]}?user_profile_id=${msisdn}&primary_msisdn=${msisdn}&secondary_msisdn=${msisdn}&circle=007&name=vaibhav&status=1`);
-    setFlagData(getFlagresponse?.data?.reward_history_flag);
-    if(getFlagresponse?.data?.reward_history_flag === 0){
-      getRewardCount();
-    }
-  }
-
   const getRewardCount = async () => {
     const rewardResponse = await getRequestData(
-      `${route["GET_REWARD_HISTORY"]}?user_profile_id=${msisdn}&spin_id=1&claim_status=0&rank=0`
+      `${route["GET_REWARD_HISTORY"]}?user_profile_id=9156576878&spin_id=1&claim_status=0&rank=0`
     );
     setRewardCount(rewardResponse?.data?.user_reward_count)
     setSpinData(rewardResponse?.data?.user_reward_count)
+  }
+
+  const getFlag = async () => {
+    const getFlagresponse = await getRequestData(`${route["GET_REWARD_HISTORY_FLAG"]}?user_profile_id=9075727698&primary_msisdn=8675786578&secondary_msisdn=9075727698&circle=0007&name=vaibhav&status=1`);
+    setFlagData(getFlagresponse?.data?.reward_history_flag)
   }
 
   const selectItem = (props) => {
     if (selectedItem === null) {
       const selectedItem = rewardCount
       let filteredItem = spinnerValues?.filter((_, i) => i == selectedItem)
-      setRewardCount(filteredItem?.map((item, i) => {
+      setRewardCount(filteredItem.map((item, i) => {
         return (
           <>
             <img src={item?.overlay_image} height={120} width={'100%'} className="mb-3 ovelayImage" />
@@ -99,7 +88,7 @@ function SpinWheel() {
                   <img src={copyIcon} height={24} width={24} />
                 </div>
                 <CopyToClipboard text={item?.coupon_code}
-                  onCopy={()=>setCopied(true)}>
+                  onCopy={copy}>
                   <p className='copyIconText'>{isCopied ? "Copied!" : "tap to copy"}</p>
                 </CopyToClipboard>
               </div>
@@ -107,7 +96,7 @@ function SpinWheel() {
           </>
         )
       }))
-      setSpinData(filteredItem?.map((item, i) => { 
+      setSpinData(filteredItem.map((item, i) => { 
         return (
           <>
             <p>{item?.description}</p>
@@ -156,7 +145,7 @@ function SpinWheel() {
         <img src={backIcon} height={25} className="backIconImage" onClick={() => navigate(-1)} />
         <p className='spinToWinHeaderText'>spin to win</p>
       </div>
-      <div className={flagData === 1 ? 'WheelWrapperdisableSpinner position-relative' : 'disableSpinner position-relative'}>
+      <div className={flagData === 1 ? 'WheelWrapperdisableSpinner' : 'disableSpinner'}>
         <div className= 'arrow' id="spinArrow" >
           <img src={spinArrowImage} />
         </div>
@@ -165,7 +154,7 @@ function SpinWheel() {
             style={wheelVars}
             onClick={selectItem}
           >
-            {(selectedItem === null || selectItem < 1) && flagData === 0 ? <div className='spinButton' onClick={startRotation}>
+            {selectedItem === null || selectItem < 1 ? <div className='spinButton' onClick={startRotation}>
               <button className='spinBtnText text-center' >SPIN</button>
             </div> :   <div className='spinButton' onClick={startRotation}>
               <button disabled className='spinBtnText text-center' >SPIN</button>
@@ -186,8 +175,8 @@ function SpinWheel() {
                         <img src={item?.logo_image} height={30} width={30} />
                       </div>
                       <div className='textWrapper text-center'>
-                        <p className='fw-bold text-truncate text-center' style={{maxWidth: '50px',margin:'0 10px'}}>{item.title}</p>
-                        <span className='text-truncate text-center d-block' style={{maxWidth: '55px',margin:'0 10px'}} >{item.sub_title}</span>
+                        <p className='fw-bold text-truncate text-center' style={{maxWidth: '50px',marginLeft:'13px'}}>{item.title}</p>
+                        <span className='text-truncate text-center' >{item.sub_title}</span>
                       </div>
                     </div>
                   </div>
