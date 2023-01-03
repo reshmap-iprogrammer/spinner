7f79b86280e72e017b02cb060fb84ba28391756a
Resham <reshmap@iprogrammer.co>
2023-01-03 14:12:46 +0530

    spin complete

--- src/App.js
+++ src/App.js
@@ -26,16 +26,16 @@ function App() {
   alert(JSON.stringify(document.location.href.split('data=').pop()))
   // alert(JSON.stringify(document.location.href.split('=')?.[1]))
   // let linkDatas = "U2FsdGVkX19sESFoX3uSxMSg9zOEHugGWVFhYpWW2hIIL5RFNzh8bXEFv5Lult9P%2BYRI%2FoX7aGvG0tYIH3ypew%3D%3D"
-  // let linkData = decodeURIComponent(linkDatas);
+  let linkData = decodeURIComponent(linkDatas);
   // console.log('object',linkData)
-  // if(linkData?.length){
-  //   let bytes = CryptoJS.AES.decrypt(linkData, 'VE1LLVNFRUQtRU5DLURFQw==')
-  //   if(bytes?.length){
-  //     let decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
-  //     alert(JSON.stringify(decryptedData))
-  //     // alert(decryptedData.msisdn)
-  //   }
-  // }
+  if(linkData?.length){
+    let bytes = CryptoJS.AES.decrypt(linkData, 'VE1LLVNFRUQtRU5DLURFQw==')
+    // if(bytes?.length){
+      let decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
+      alert(JSON.stringify(decryptedData))
+      // alert(decryptedData.msisdn)
+    // }
+  }
 
 
   const btnClick = async () => {
