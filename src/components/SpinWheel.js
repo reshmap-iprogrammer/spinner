9db9607c86d9c8723ba6f4c85b3115e8a5bb6a80
Resham <reshmap@iprogrammer.co>
2023-01-03 14:24:51 +0530

    spin complete

--- src/components/SpinWheel.js
+++ src/components/SpinWheel.js
@@ -32,7 +32,7 @@ function SpinWheel() {
   let linkData = decodeURIComponent(linkDatas);
   if(linkData?.length){
     let bytes = CryptoJS.AES.decrypt(linkData, 'VE1LLVNFRUQtRU5DLURFQw==')
-    if(bytes?.length){
+    if(bytes){
       let decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
       msisdn = JSON.stringify(decryptedData)
       alert(JSON.stringify(decryptedData))
