636fb1b106bd14dddf93357e9fb2b44270421292
Resham <reshmap@iprogrammer.co>
2023-01-04 15:31:22 +0530

    spin complete

--- src/components/CommonModal.js
+++ src/components/CommonModal.js
@@ -11,7 +11,7 @@ function CommonModal({ showModal, toggle, spinnerValue, image, spinData, flagDat
     if (window.ReactNativeWebView) {
       window.ReactNativeWebView.postMessage(spinData[0]?.props?.children?.props?.children)
     }
-    setLoading(false);
+    // setLoading(false);
   }
   return (
     <div>
