62ba931c297c45344269f0496880af16d89a0641
Resham <reshmap@iprogrammer.co>
2023-01-09 19:46:02 +0530

    spin complete

--- src/components/CommonModal.js
+++ src/components/CommonModal.js
@@ -27,11 +27,11 @@ function CommonModal({ showModal, toggle, spinnerValue, flagData, rewardDesc, be
   const claimReaward = () => {
     alert(2)
     // const obj1 = Object.assign({}, newArr);
-    if (window.ReactNativeWebView) {
-      window.ReactNativeWebView.postMessage(encodeToken);
-      // window.ReactNativeWebView.postMessage(JSON.stringify(obj1));
-      // window.ReactNativeWebView.postMessage(benefit[0]?.props?.children?.props?.children)
-    }
+    // if (window.ReactNativeWebView) {
+    //   window.ReactNativeWebView.postMessage(encodeToken);
+    //   // window.ReactNativeWebView.postMessage(JSON.stringify(obj1));
+    //   // window.ReactNativeWebView.postMessage(benefit[0]?.props?.children?.props?.children)
+    // }
   }
 
   const AppToWeb = () => {
