f5fbc6affe53a5a626bdcee6dd3301c5af012c8b
Resham <reshmap@iprogrammer.co>
2023-01-09 21:02:00 +0530

    spin complete

--- src/App.js
+++ src/App.js
@@ -16,22 +16,22 @@ import CryptoJS from "crypto-js";
 function App() {
 const [appToWeb, setappToWeb] = useState()
 
-  let msisdn;
-  let parentMsisdn;
-  let circleId;
-  let linkDatas = document.location.href.split('=')?.[1]
-  if(linkDatas){
-    let linkData = decodeURIComponent(linkDatas);
-    let bytes = CryptoJS.AES.decrypt(linkData, 'VE1LLVNFRUQtRU5DLURFQw==')
-      let decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
-      msisdn = JSON.parse(decryptedData.msisdn)
-      parentMsisdn = JSON.parse(decryptedData.parentMsisdn)
-      circleId = JSON.parse(decryptedData.circleId)
-  }
+  // let msisdn;
+  // let parentMsisdn;
+  // let circleId;
+  // let linkDatas = document.location.href.split('=')?.[1]
+  // if(linkDatas){
+  //   let linkData = decodeURIComponent(linkDatas);
+  //   let bytes = CryptoJS.AES.decrypt(linkData, 'VE1LLVNFRUQtRU5DLURFQw==')
+  //     let decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
+  //     msisdn = JSON.parse(decryptedData.msisdn)
+  //     parentMsisdn = JSON.parse(decryptedData.parentMsisdn)
+  //     circleId = JSON.parse(decryptedData.circleId)
+  // }
 
-  alert(msisdn)
-  alert(parentMsisdn)
-  alert(circleId)
+  // alert(msisdn)
+  // alert(parentMsisdn)
+  // alert(circleId)
 
   const btnClick = async () => {
     const rewardResponse = await getRequestData(route["GET_SPIN"]);
@@ -48,19 +48,19 @@ const [appToWeb, setappToWeb] = useState()
   // }, [])
   
 
-  // const AppToWeb = () => {
-  //   alert(1)
-  //   document.addEventListener("message", function (data) {
-  //     alert(2)
-  //     setTimeout(() => {
-  //       alert(data.data);
-  //     }, 3000);
-  //   });
-  // }
+  const AppToWeb = () => {
+    alert(1)
+    document.addEventListener("message", function (data) {
+      alert(2)
+      setTimeout(() => {
+        alert(data.data);
+      }, 3000);
+    });
+  }
 
   return (
     <Router>
-      {/* <button onClick={AppToWeb}>Hi</button> */}
+      <button onClick={AppToWeb}>Hi</button>
       <Routes>
         {/* <Route path="/" element={<SpinWheel tagline={''} msisdn={msisdn} parentMsisdn={parentMsisdn} circleId={circleId}/>} />
         <Route path="/rewardHistory" element={<RewardHistory  msisdn={msisdn}/>} />
