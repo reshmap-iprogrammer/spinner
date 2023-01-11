cf822996e7f7085c4cf7cb9123c570004abed035
Resham <reshmap@iprogrammer.co>
2023-01-11 10:25:12 +0530

    spin complete

--- src/components/CommonModal.js
+++ src/components/CommonModal.js
@@ -18,12 +18,10 @@ function CommonModal({ showModal, toggle, spinnerValue, flagData, spinnerValues,
 
   useEffect(() => {
     document.addEventListener("message", function (data) {
-      alert(data.data)
       const appData = data?.data
       let AppData = decodeURIComponent(appData);
       let rewardBytes = CryptoJS.AES.decrypt(AppData, "SE1LLVNSRUQtRU5DLURFQw==")
       let decryptedData = JSON.parse(rewardBytes.toString(CryptoJS.enc.Utf8))
-      alert(decryptedData)
       if(decryptedData !== undefined){
         localStorage.setItem("dummy", decryptedData)
       } 
