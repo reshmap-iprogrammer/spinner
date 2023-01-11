115556057cf3a01e37930ce2317502e32d0b9e1d
Resham <reshmap@iprogrammer.co>
2023-01-11 12:31:28 +0530

    spin complete

--- src/components/CommonModal.js
+++ src/components/CommonModal.js
@@ -24,7 +24,7 @@ let decryptedData
       let rewardBytes = CryptoJS.AES.decrypt(AppData, "SE1LLVNSRUQtRU5DLURFQw==")
        decryptedData = JSON.parse(rewardBytes.toString(CryptoJS.enc.Utf8))
       // if(decryptedData !== undefined){
-      dxlclaimStatus = localStorage.setItem("dummy", JSON.stringify(decryptedData))
+      dxlclaimStatus = localStorage.setItem("dummy", decryptedData)
       alert(dxlclaimStatus)
       // } 
       setLoading(false);
